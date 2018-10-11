8a1edbc8565190e0ccb22d47bbb56153505ed617
prudhviraj3884 <chprudhviraj1998@gmail.com>
2018-10-12 04:53:09 +0530

    th

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnterName = new System.Windows.Forms.Label();
+            this.EnterPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // EnterName
+            // 
+            this.EnterName.AutoSize = true;
+            this.EnterName.ForeColor = System.Drawing.SystemColors.Info;
+            this.EnterName.Location = new System.Drawing.Point(253, 98);
+            this.EnterName.Name = "EnterName";
+            this.EnterName.Size = new System.Drawing.Size(78, 13);
+            this.EnterName.TabIndex = 0;
+            this.EnterName.Text = "ENTER NAME";
+            // 
+            // EnterPassword
+            // 
+            this.EnterPassword.AutoSize = true;
+            this.EnterPassword.ForeColor = System.Drawing.SystemColors.Info;
+            this.EnterPassword.Location = new System.Drawing.Point(253, 144);
+            this.EnterPassword.Name = "EnterPassword";
+            this.EnterPassword.Size = new System.Drawing.Size(110, 13);
+            this.EnterPassword.TabIndex = 1;
+            this.EnterPassword.Text = "ENTER PASSWORD";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(411, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(411, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EnterPassword);
+            this.Controls.Add(this.EnterName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EnterName;
+        private System.Windows.Forms.Label EnterPassword;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
